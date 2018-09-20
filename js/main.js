218133a366fcaae92f3c4bad651a6407034b66f2
martinblevy <dev@martinblevy.com>
2018-09-20 11:19:20 +0200

    Stylisation et agencement du carousel des actualités / Encore de petits soucis avec le responsive

--- js/main.js
+++ js/main.js
@@ -120,5 +120,26 @@ var   ylwbtn = $("#ylw-btn"),
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 3,
+  dots: true,
+  infinite: true,
+  responsive: [
+  {
+      breakpoint: 1025,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
